797d282525dbb262ffe0d5d769c1870c71c45308
boukrana <boukranahabib@gmail.com>
2022-08-11 19:15:41 +0200

    create Models

--- commande/models.py
+++ commande/models.py
@@ -1,3 +1,12 @@
+from random import choices
 from django.db import models
 
 # Create your models here.
+class Commande(models.Model):
+    STATUS=(('en instance','en instance'),
+    ('non livré','non livré'),
+    ('livré','livré'))
+    #client = models.CharField() 
+    #produit = 
+    status = models.CharField(max_length=200,null=True,choices=STATUS)
+    date_creation = models.DateTimeField(auto_now_add=True, null=True)
